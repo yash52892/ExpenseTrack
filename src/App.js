b8e437ce060d1ae5d046a0854485de7eadad12d2
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-01-18 23:42:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Forms from './components/Form';
 import Home from './pages/Home';
+import Profile from './pages/Profile';
+import TokenProvider from './Store/TokenProvider';
 
 function App() {
   return (
+    <TokenProvider>
     <Router>
       <Routes>
         <Route path='/' element={<Forms/>}/>
         <Route path='/home' element={<Home/>}/>
+        <Route path='/profile' element={<Profile/>}/>
       </Routes>
     </Router>
+    </TokenProvider>
   );
 }
 
