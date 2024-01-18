77bfa9cf980104b9e1be21a1195174cb3107a17b
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-01-19 00:13:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import TokenProvider from './Store/TokenProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <TokenProvider>
     <App />
+  </TokenProvider>
   </React.StrictMode>
 );
 
