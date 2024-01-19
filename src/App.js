e91795abadc11d85bca079bbebc5283bf0263a24
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-01-20 00:21:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Forms from './components/Form';
+import Forms from './components/Authentication/Form';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Token from './Store/TokenContext';
 import { useContext } from 'react';
 import User from './pages/User';
+import Forgot from './components/Authentication/Forgot';
+import ExpenseForm from './components/Expense/ExpenseForm';
 
 function App() {
   const tok=useContext(Token);
-  console.log(tok);
   return (
     <Router>
       <Routes>
@@ -19,7 +20,9 @@ function App() {
          (<Navigate replace to="/" element={ <Forms />}/>)}/>
          <Route path='/user' element={tok.isLoggedin ? (<User/>):
          (<Navigate replace to="/" element={ <Forms />}/>)}/>
-          <Route path='/forgot' element={<Forgot/>}/>
+         <Route path='/expense' element={tok.isLoggedin ? (<ExpenseForm/>):
+         (<Navigate replace to="/" element={ <Forms />}/>)}/>
+         <Route path='/forgot' element={<Forgot/>}/>
       </Routes>
     </Router>
   );
