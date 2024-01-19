2a03333df3da990c97d2e5fe7cf394ba473b0b33
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-01-19 12:04:46 +0530

    user page to show the details

---
+++ src/pages/User.js
@@ -0,0 +1,18 @@
+const User = () => {
+  return (
+    <div class="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+      <div class="relative mx-4 mt-4 h-80 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg">
+        <img
+          src="https://www.material-tailwind.com/img/team-3.jpg"
+          alt="profile"
+        />
+      </div>
+      <div class="p-6 text-center">
+        <h4 class="mb-2 block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+          Natalie Paisley
+        </h4>
+      </div>
+    </div>
+  );
+};
+export default User;
