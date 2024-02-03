be49da63436364882f97a500a532bc95126a7058
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-02-04 01:09:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import Forms from './components/Authentication/Form';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
-import Token from './Store/TokenContext';
-import { useContext } from 'react';
+import {useSelector} from 'react-redux';
 import User from './pages/User';
 import Forgot from './components/Authentication/Forgot';
 import ExpenseForm from './components/Expense/ExpenseForm';
 
+
 function App() {
-  const tok=useContext(Token);
+
+  const tok= useSelector(state=>state.auth.isLogged);
   return (
     <Router>
       <Routes>
-        {!tok.isLoggedin && <Route path='/' element={<Forms/>}/>}
-        <Route path='/home' element={tok.isLoggedin ? (<Home/>) :
+        <Route path='/' element={tok ? (<Home/>) :
            (<Navigate replace to="/" element={ <Forms />}/>)}/>
-        <Route path='/profile' element={tok.isLoggedin ? (<Profile/>):
+        <Route path='/profile' element={tok ? (<Profile/>):
          (<Navigate replace to="/" element={ <Forms />}/>)}/>
-         <Route path='/user' element={tok.isLoggedin ? (<User/>):
+         <Route path='/user' element={tok ? (<User/>):
          (<Navigate replace to="/" element={ <Forms />}/>)}/>
-         <Route path='/expense' element={tok.isLoggedin ? (<ExpenseForm/>):
+         <Route path='/expense' element={tok ? (<ExpenseForm/>):
          (<Navigate replace to="/" element={ <Forms />}/>)}/>
          <Route path='/forgot' element={<Forgot/>}/>
       </Routes>
