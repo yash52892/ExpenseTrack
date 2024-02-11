d53086578b0d4081afbfe3670539807e05097261
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-02-11 16:26:02 +0530

    Add files via upload

---
+++ src/pages/Layout.js
@@ -0,0 +1,102 @@
+import { Outlet, Link, useNavigate } from "react-router-dom";
+import { MdOutlineDarkMode } from "react-icons/md";
+import { MdOutlineLightMode } from "react-icons/md";
+import { setMode } from "../Redux/Slice/theme";
+import { useDispatch, useSelector } from "react-redux";
+import LogOut from "../Redux/Slice/auth";
+
+const Layout = () => {
+  const dispatch = useDispatch();
+  const { darkMode } = useSelector((state) => state.mode);
+  const expense = useSelector((state) => state.expense.expense);
+
+  const handleLogout = () => {
+    dispatch(LogOut());
+  };
+
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    
+    const blob1=new Blob([expense], {type: "csv"});
+    link.href = URL.createObjectURL(blob1);
+    link.download = 'Example.csv';
+    link.click();
+  };
+
+  return (
+    <>
+      <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-5">
+        <div class="flex items-center flex-shrink-0 text-white">
+          <svg
+            class="fill-current h-8 w-8 mr-2"
+            width="54"
+            height="54"
+            viewBox="0 0 54 54"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
+          </svg>
+          <span class="font-semibold text-xl tracking-tight">
+            React Expense{" "}
+          </span>
+        </div>
+        <div className="text-xl flex mr-7">
+          <Link
+            to="/home"
+            className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+          >
+            {" "}
+            Home{" "}
+          </Link>
+          <Link
+            to="/profile"
+            className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+          >
+            {" "}
+            Profile{" "}
+          </Link>
+          <Link
+            to="/expense"
+            className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+          >
+            {" "}
+            Expense{" "}
+          </Link>
+          <Link
+            to="/sign"
+            onClick={handleLogout}
+            className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+          >
+            {" "}
+            Log out{" "}
+          </Link>
+        </div>
+        <div class="flex">
+          <section
+            className={`${
+              darkMode ? "text-richblack-100 pl-5" : "text-richblack-700 pl-5"
+            }`}
+            onClick={() => {
+              dispatch(setMode(!darkMode));
+            }}
+          >
+            {darkMode ? (
+              <MdOutlineLightMode size={"35px"} />
+            ) : (
+              <MdOutlineDarkMode size={"35px"} />
+            )}
+          </section>
+          <button
+            class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+            onClick={handleDownload}
+          >
+            Download
+          </button>
+        </div>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
+
+export default Layout;
