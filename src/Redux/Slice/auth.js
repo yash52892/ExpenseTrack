9b435bae2d695abc24146ca7b72e523b55b9cda3
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-02-15 22:38:07 +0530

    Update auth.js

--- src/Redux/Slice/auth.js
+++ src/Redux/Slice/auth.js
@@ -1,21 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
+import {LogIn}  from './authAction.js';
+import { useNavigate } from "react-router-dom";
 
-const initAuthState = { isLogged: true, id: null };
+const initAuthState = { isLogged: false};
 
 const authSlice = createSlice({
     name: "auth",
     initialState: initAuthState,
-    reducers: {
-      login(state, action) {
-        state.isLogged = true;
-        state.id = action.payload;
-      },
-      logout(state) {
-        state.isLogged = false;
-        state.id = null;
-      },
+    extraReducers: builder => {
+      builder.addCase(LogIn.fulfilled, (state, action) => {
+        localStorage.setItem("id",action.payload.idToken)
+        const id=localStorage.getItem("id");
+        state.isLogged=!!id;
+       
+      })
+      builder.addCase(LogIn.rejected, (state, action) => {
+        state.loading = false
+        state.error = action
+      })
     },
+    reducers: {
+      LogOut: (state)=>{
+        const nav = useNavigate();
+        state.id=null
+        localStorage.removeItem("id");
+        state.isLogged=false;
+        nav("/");
+      }
+    }
   });
-
+  
   export default authSlice.reducer;
-  export const authActions = authSlice.actions;
+  export const LogOut = authSlice.actions;
+  
+  
