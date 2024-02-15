4e32f80b8c1ce283d602abb4e95cdf4d5489da36
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-02-15 22:38:44 +0530

    Update Profile.js

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,17 +1,18 @@
 import { useEffect, useRef, useState } from "react";
-import {useSelector} from 'react-redux';
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
   const[n, setName]=useState(null);
   const[url,setUrl]=useState(null);
 
   const Dname = useRef(null);
-  const photo = useRef(null);
+  const photo = useRef(null);   
 
   const navigate=useNavigate();
 
-  const tokid=useSelector(state=>state.auth.id);
+  const tokid=localStorage.getItem("id");
+  const darkMode=useSelector(state=>state.mode.darkMode)
 
   useEffect( ()=>{
     const fetching= async ()=>{
@@ -23,8 +24,10 @@ const Profile = () => {
             })
         });
         const data=await result.json();
-        setName(data.users[0].displayName);
-        setUrl(data.users[0].photoUrl);
+        if(data){
+          setName(data.users.displayName);
+          setUrl(data.users.photoUrl);
+        }
       };
       fetching();
   },[tokid])
@@ -54,78 +57,81 @@ const Profile = () => {
   }
 
   return (
-    <div class="items-center bg-gray-400">
+    <div className={`transition-all  
+    duration-500  
+    ease-in-out  
+    ${darkMode ?  
+    "bg-black " :  
+    "bg-white"}`}>
       <form onSubmit={handleprofile}>
-        <div class="space-y-12">
-          <div class="border-b border-gray-900/10 pb-12 justify-items-center mx-8">
-            <h2 class="text-xl text-base font-semibold leading-7 text-gray-900">
+        <div className="space-y-12">
+          <div className="border-b border-gray-900/10 pb-12 justify-items-center mx-8">
+            <h2 className="text-xl text-base font-semibold leading-7 text-red-900">
               Profile
             </h2>
-            <div class="col-span-full">
-              <div class="mt-2 flex items-center gap-x-3">
+            <div className="col-span-full">
+              <div className="mt-2 flex items-center gap-x-3">
               </div>
             </div>
           </div>
         </div>
 
-        <div class="border-b border-gray-900/10 pb-12 items-center">
-          <h2 class="text-base font-semibold leading-7 text-gray-900">
+        <div className="border-b border-gray-900/10 pb-12 items-center">
+          <h2 className="text-base font-semibold leading-7 text-red-900">
             Personal Information
           </h2>
-          <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <div class="sm:col-span-3 items-center">
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-3 items-center">
               <label
-                for="first-name"
-                class="block text-sm font-medium leading-6 text-gray-900"
+                htmlFor="first-name"
+                className="block text-sm font-medium leading-6 text-red-900"
               >
                 First name
               </label>
-              <div class="mt-2">
+              <div className="mt-2">
                 <input
                   type="text"
                   name="first-name"
                   id="first-name"
                   ref={Dname}
                   value={n}
-                  autocomplete="given-name"
-                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-red-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
 
-            <div class="sm:col-span-3">
+            <div className="sm:col-span-3">
               <label
-                for="last-name"
-                class="block text-sm font-medium leading-6 text-gray-900"
+                htmlFor="last-name"
+                className="block text-sm font-medium leading-6 text-red-900"
               >
                 Profile Photo URL
               </label>
-              <div class="mt-2">
+              <div className="mt-2">
                 <input
                   type="text"
                   name="last-name"
                   id="last-name"
                   ref={photo}
                   value={url}
-                  autocomplete="family-name"
-                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
           </div>
         </div>
 
-        <div class="mt-6 flex items-center justify-end gap-x-6">
+        <div className="mt-6 flex items-center justify-end gap-x-6">
           <button
             type="button"
-            class="text-sm font-semibold leading-6 text-gray-900"
+            className="text-sm font-semibold leading-6 text-red-900"
             onClick={handlecancel}
           >
             Cancel
           </button>
           <button
             type="submit"
-            class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-red shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Save
           </button>
