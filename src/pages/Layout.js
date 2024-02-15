7ccdfd9e9bdad80eccc8ca98e1df6abd8a4dec2e
Yashwanth kumar R <97838432+yash52892@users.noreply.github.com>
2024-02-15 22:39:03 +0530

    Update Layout.js

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,4 +1,4 @@
-import { Outlet, Link, useNavigate } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 import { MdOutlineDarkMode } from "react-icons/md";
 import { MdOutlineLightMode } from "react-icons/md";
 import { setMode } from "../Redux/Slice/theme";
@@ -13,22 +13,30 @@ const Layout = () => {
   const handleLogout = () => {
     dispatch(LogOut());
   };
-
+  let total=0,premium=false;
+  if(expense){
+    expense.map((item)=>total=total+parseInt(item.amount))
+    if(total>10000){
+      premium=true
+    }
+  }
+  
   const handleDownload = () => {
     const link = document.createElement('a');
-    
+    if(expense){
     const blob1=new Blob([expense], {type: "csv"});
     link.href = URL.createObjectURL(blob1);
     link.download = 'Example.csv';
     link.click();
+    }
   };
 
   return (
     <>
-      <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-5">
-        <div class="flex items-center flex-shrink-0 text-white">
+      <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-5">
+        <div className="flex items-center flex-shrink-0 text-white">
           <svg
-            class="fill-current h-8 w-8 mr-2"
+            className="fill-current h-8 w-8 mr-2"
             width="54"
             height="54"
             viewBox="0 0 54 54"
@@ -36,7 +44,7 @@ const Layout = () => {
           >
             <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
           </svg>
-          <span class="font-semibold text-xl tracking-tight">
+          <span className="font-semibold text-xl tracking-tight">
             React Expense{" "}
           </span>
         </div>
@@ -71,8 +79,8 @@ const Layout = () => {
             Log out{" "}
           </Link>
         </div>
-        <div class="flex">
-          <section
+        <div className="flex">
+          {premium?(<section
             className={`${
               darkMode ? "text-richblack-100 pl-5" : "text-richblack-700 pl-5"
             }`}
@@ -85,13 +93,12 @@ const Layout = () => {
             ) : (
               <MdOutlineDarkMode size={"35px"} />
             )}
-          </section>
-          <button
-            class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
-            onClick={handleDownload}
-          >
+          </section>):<></>}
+          {premium? (<button
+            className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+            onClick={handleDownload}>
             Download
-          </button>
+          </button>):(<div></div>)}
         </div>
       </nav>
       <Outlet />
